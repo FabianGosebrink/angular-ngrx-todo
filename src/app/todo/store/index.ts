f8cb17d3456652d7a0e6c8ba3c68210ae526cc44
FabianGosebrink <fabian.gosebrink@outlook.com>
2019-05-09 22:14:21 +0200

    added new selectors file

--- src/app/todo/store/index.ts
+++ src/app/todo/store/index.ts
@@ -1,35 +1,4 @@
-import {
-  ActionReducerMap,
-  createFeatureSelector,
-  createSelector
-} from '@ngrx/store';
-import { ReducerTodoState, todoReducer } from './todo.reducer';
-
-export const featureStateName = 'todoFeature';
-
-export interface TodoState {
-  todo: ReducerTodoState;
-}
-
-export const todoReducers: ActionReducerMap<TodoState> = {
-  todo: todoReducer
-};
-
-export const getTodoFeatureState = createFeatureSelector<TodoState>(
-  featureStateName
-);
-
-export const getAllUndoneItems = createSelector(
-  getTodoFeatureState,
-  (state: TodoState) => state.todo.items.filter(x => !x.done)
-);
-
-export const getAllDoneItems = createSelector(
-  getTodoFeatureState,
-  (state: TodoState) => state.todo.items.filter(x => x.done)
-);
-
-export const getSelectedItem = createSelector(
-  getTodoFeatureState,
-  (state: TodoState) => state.todo.selectedItem
-);
+export * from './todo.actions';
+export * from './todo.effects';
+export * from './todo.reducer';
+export * from './todo.selectors';
